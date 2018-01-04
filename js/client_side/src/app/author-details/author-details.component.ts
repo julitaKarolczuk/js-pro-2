c91aa431dca1116177e2057abb873be9c57faf70
Rafał Czajkowski <czajeczkaaa@gmail.com>
2018-01-04 20:53:49 +0100

    change make by Olcix

--- js/client_side/src/app/author-details/author-details.component.ts
+++ js/client_side/src/app/author-details/author-details.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import {Author} from '../model/author';
 
 @Component({
   selector: 'app-author-details',
   templateUrl: './author-details.component.html',
   styleUrls: ['./author-details.component.css']
 })
+
 export class AuthorDetailsComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+
+  }
+  getAuthor(authorId){
+
   }
 
 }
--- js/client_side/src/app/authors/authors.component.ts
+++ js/client_side/src/app/authors/authors.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import {Author} from '../model/author';
+import {AuthorsService} from '../service/authors.service';
 
 @Component({
   selector: 'app-authors',
   templateUrl: './authors.component.html',
   styleUrls: ['./authors.component.css']
 })
+
 export class AuthorsComponent implements OnInit {
 
-  constructor() { }
+	allAuthors: Author[]=[];
+
+	selectedAuthor : Author;
+
+	onSelect(author:Author) : void{
+	this.selectedAuthor = author;
+	}
+
+  constructor(private authorsService: AuthorsService) { }
 
   ngOnInit() {
+  this.getAuthors();
   }
-
+getAuthors(){
+	this.authorsService.getAllAuthors().subscribe(data => this.allAuthors = data, error => console.log("Wystąpił błąd"));
+}
 }
